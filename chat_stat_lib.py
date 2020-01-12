73ced038e4b80bfcca9fd4fac4d5e3003786ad9a
andrey18106 <andrey18106x@gmail.com>
2020-01-12 11:33:24 +0200

    Minor fixes

--- chat_stat_lib.py
+++ chat_stat_lib.py
@@ -1,13 +1,23 @@
-# Количество сообщений от пользователя (name)
+# coding=utf-8
+
+# Количество сообщений от пользователя (from_id)
 def count_msg_from(from_id, msg_dict):
     msg_counter = 0
     
     for i in range(0, len(msg_dict) - 1):
         message = msg_dict[i]
-        if message['from_id'] == from_id:
+        if message["from_id"] == from_id:
             msg_counter += 1
 
     return msg_counter
+
+# Количество символов в сообщениях от пользователя (from_id)
+def count_msg_symbols(from_id, msg_dict):
+    pass 
+
+# Получить from_id по имени (названию чата)
+def get_from_id(name, msg_dict):
+    pass
 
 # Вывод корректного сообщения о количестве сообщений =)
 def print_msg_count(name, count):
@@ -16,8 +26,8 @@ def print_msg_count(name, count):
     last_number = int(str_count[str_count_last_index])
 
     if last_number in range(2, 4):
-        print(name, count, 'сообщения')
+        print(name, count, "сообщения")
     elif last_number in range(5, 9) or last_number == 0:
-        print(name, count, 'сообщений')
+        print(name, count, "сообщений")
     elif last_number == 1:
-        print(name, count, 'сообщение')
+        print(name, count, "сообщение")
--- tg_chat_stat.py
+++ tg_chat_stat.py
@@ -1,50 +1,52 @@
+# coding=utf-8
+
 import json
 import chat_stat_lib as lib
 
 # Path to file with data from Telegram
-path_to_file = 'C:/Users/Андрей/Downloads/Telegram Desktop/DataExport_11_01_2020(JSON)/result.json'
+path_to_file = "C:/Users/Андрей/Downloads/Telegram Desktop/DataExport_11_01_2020(JSON)/result.json"
 
-with open(path_to_file, 'r', encoding='utf8') as file:
+with open(path_to_file, "r", encoding="utf8") as file:
     data = json.load(file)
 
-user = data['personal_information'] # Информация о пользователе (чей экспорт)
-print('Exported data of ' + user['first_name'] + ' ' + user['last_name'] + ' (' + user['username'] + ')' + ' from Telegram successfully load!')
+user = data["personal_information"] # Информация о пользователе (чей экспорт)
+print("Exported data of " + user["first_name"] + " " + user["last_name"] + " (" + user["username"] + ")" + " from Telegram successfully load!")
 
-chat_list = data['chats']['list'] # Все данные чатов из файла
+chat_list = data["chats"]["list"] # Все данные чатов из файла
 chat_names = dict() # Список чатов
 counter = 0 # Счетчик чатов
 
 for i in range(0, len(chat_list) - 1):
-    if chat_list[i]['type'] == 'personal_chat': # Если это ЛС (диалог)
+    if chat_list[i]["type"] == "personal_chat": # Если это ЛС (диалог)
         counter += 1
         chat_names[counter] = {
             "number": i,
-            "name": chat_list[i]['name'] # Получаем названия чатов и заносим в другой список
+            "name": chat_list[i]["name"] # Получаем названия чатов и заносим в другой список
         }
 
 is_active = True
 # Цикл работы программулины
 while is_active:
     
-    print('\nВыберите действие:')
-    print('1. Выбрать чат')
-    print('0. Выход')
+    print("\nВыберите действие:")
+    print("1. Выбрать чат")
+    print("0. Выход")
 
-    _input = input('-> ')
+    _input = input("-> ")
 
     if int(_input) == 1:
         
-        print('Список чатов:')
+        print("Список чатов:")
         for key in chat_names:
-            print(key, chat_names[key]['name'])
+            print(key, chat_names[key]["name"])
         
-        selected_chat = input('Введите номер чата для получения информации:\n-> ')
-        selected_chat_number = chat_names[int(selected_chat)]['number'] # Выбранный номер чата, соответствующий номеру в файле
-        selected_chat_dict = chat_list[selected_chat_number]['messages'] # Словарь сообщений выбраного чата
+        selected_chat = input("Введите номер чата для получения информации:\n-> ")
+        selected_chat_number = chat_names[int(selected_chat)]["number"] # Выбранный номер чата, соответствующий номеру в файле
+        selected_chat_dict = chat_list[selected_chat_number]["messages"] # Словарь сообщений выбраного чата
 
-        lib.print_msg_count(chat_list[selected_chat_number]['name'], len(chat_list[selected_chat_number]['messages']))
-        print('Количество сообщений от ' + user['first_name'] + ' ' + user['last_name'] + ':', lib.count_msg_from(user['user_id'], selected_chat_dict))
-        print('Количество сообщений от ' + chat_list[selected_chat_number]['name'] + ':', len(chat_list[selected_chat_number]['messages']) - lib.count_msg_from(user['user_id'], selected_chat_dict))
+        lib.print_msg_count(chat_list[selected_chat_number]["name"], len(chat_list[selected_chat_number]["messages"]))
+        print("Количество сообщений от " + user["first_name"] + " " + user["last_name"] + ":", lib.count_msg_from(user["user_id"], selected_chat_dict))
+        print("Количество сообщений от " + chat_list[selected_chat_number]["name"] + ":", len(chat_list[selected_chat_number]["messages"]) - lib.count_msg_from(user["user_id"], selected_chat_dict))
 
     elif int(_input) == 0:
         is_active = False
