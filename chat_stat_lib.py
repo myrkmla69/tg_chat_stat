9b90904081215626d1e5ae835ff0e2a98b5f15e6
andrey18106 <andrey18106x@gmail.com>
2020-01-12 16:59:47 +0200

    Some fixes, add searching export files (json)

--- chat_stat_lib.py
+++ chat_stat_lib.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+import os
+
 # Количество сообщений от пользователя (from_id)
 def count_msg_from(from_id, msg_dict):
     msg_counter = 0
@@ -19,15 +21,30 @@ def count_msg_symbols(from_id, msg_dict):
 def get_from_id(name, msg_dict):
     pass
 
-# Вывод корректного сообщения о количестве сообщений =)
-def print_msg_count(name, count):
+# Получение корректного сообщения о количестве сообщений =)
+def get_msg_count(count):
+    if count is None:
+        return ""
+
     str_count = str(count)
-    str_count_last_index = len(str_count) - 1
-    last_number = int(str_count[str_count_last_index])
+    last_number = int(str_count[len(str_count) - 1])
 
     if last_number in range(2, 4):
-        print(name, count, "сообщения")
+        return "сообщения"
     elif last_number in range(5, 9) or last_number == 0:
-        print(name, count, "сообщений")
+        return "сообщений"
     elif last_number == 1:
-        print(name, count, "сообщение")
+        return "сообщение"
+
+
+def get_download_path():
+    """Returns the default downloads path for linux or windows"""
+    if os.name == 'nt':
+        import winreg
+        sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
+        downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
+            location = winreg.QueryValueEx(key, downloads_guid)[0]
+        return location
+    else:
+        return os.path.join(os.path.expanduser('~'), 'downloads')
--- tg_chat_stat.py
+++ tg_chat_stat.py
@@ -1,10 +1,37 @@
 # coding=utf-8
 
+import os
+import re
 import json
+import time
 import chat_stat_lib as lib
 
 # Path to file with data from Telegram
-path_to_file = "C:/Users/Андрей/Downloads/Telegram Desktop/DataExport_11_01_2020(JSON)/result.json"
+path_to_file = "C:/Users/Андрей/Downloads/Telegram Desktop/DataExport_12_01_2020/result.json"
+
+if os.name == "nt":
+    tg_download_path = lib.get_download_path() + "\Telegram Desktop" # Директория загрузок Telegram по-умолчанию
+    tg_download_dirs = dict() # Список директорий
+    tg_dir_count = 0 # Счетчик директорий
+
+    if os.path.exists(tg_download_path): # Если данная директория существует
+        for directory in os.listdir(tg_download_path): 
+            if os.path.isdir(tg_download_path + "\\" + directory): # Отбираем только директории
+                if re.match("DataExport_+", directory): # Если это директории экспорта данных
+                    if os.listdir(tg_download_path + "\\" + directory).count("result.json") == 1:
+                        tg_dir_count += 1
+                        tg_download_dirs[tg_dir_count] = directory # Добавляем в список
+
+    print(f"Найдено файлов экспорта (json): {tg_dir_count}.")
+    print("Выберите экспорт:")
+
+    for i in range(1, tg_dir_count + 1):
+        print(str(i) + ". " + tg_download_dirs[i])
+
+    selected_export_file = int(input("-> "))
+
+    if selected_export_file in range(1, tg_dir_count):
+        path_to_file = tg_download_path + "\\" + tg_download_dirs[selected_export_file] + "\\result.json"
 
 with open(path_to_file, "r", encoding="utf8") as file:
     data = json.load(file)
@@ -38,15 +65,18 @@ while is_active:
         
         print("Список чатов:")
         for key in chat_names:
-            print(key, chat_names[key]["name"])
+            print(str(key) + '.', chat_names[key]["name"])
         
         selected_chat = input("Введите номер чата для получения информации:\n-> ")
+
         selected_chat_number = chat_names[int(selected_chat)]["number"] # Выбранный номер чата, соответствующий номеру в файле
         selected_chat_dict = chat_list[selected_chat_number]["messages"] # Словарь сообщений выбраного чата
+        selected_chat_count = len(chat_list[selected_chat_number]["messages"])
 
-        lib.print_msg_count(chat_list[selected_chat_number]["name"], len(chat_list[selected_chat_number]["messages"]))
+        print("\nСтатистика по выбранному чату (", chat_names[int(selected_chat)]["name"], "):\n")
+        print("Всего", selected_chat_count, lib.get_msg_count(selected_chat_count))
         print("Количество сообщений от " + user["first_name"] + " " + user["last_name"] + ":", lib.count_msg_from(user["user_id"], selected_chat_dict))
-        print("Количество сообщений от " + chat_list[selected_chat_number]["name"] + ":", len(chat_list[selected_chat_number]["messages"]) - lib.count_msg_from(user["user_id"], selected_chat_dict))
+        print("Количество сообщений от " + chat_list[selected_chat_number]["name"] + ":", selected_chat_count - lib.count_msg_from(user["user_id"], selected_chat_dict))
 
     elif int(_input) == 0:
         is_active = False
